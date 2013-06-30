07210b664e09b2e1ef647070b24cc6c09db99996
Russell Branca <chewbranca@gmail.com>
2013-06-29 22:17:31 -0700

    Initial commit

---
+++ src/notifierl_app.erl
@@ -0,0 +1,16 @@
+-module(notifierl_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    notifierl_sup:start_link().
+
+stop(_State) ->
+    ok.
